aef3c136524c5f13c5bb0537ad5e04d69205e57c
Henrikgg <Henrik_gg94@hotmail.com>
2025-02-28 00:07:03 +0100

    List innlegg i ListInnlegg component

--- index.jsx
+++ index.jsx
@@ -3,6 +3,21 @@ import * as ReactDOM from 'react-dom';
 import { Route, Link, BrowserRouter, Routes} from "react-router-dom";
 
 
+const innlegg = [
+    {
+        title: "Hun sa dette omg!..",
+        plot: "Det hele starten med.....",
+        year: 2025
+    },
+    {
+        title: "Han sa dette omg!..",
+        plot: "Det hele started med....",
+        year: 2024
+    }
+]
+
+
+
 function FrontPage() {
         return <div>
             <>Sosiale Medier Database</>
@@ -13,12 +28,28 @@ function FrontPage() {
         </div>;
 }
 
+
+
+
+
+function ListInnlegg()  {
+        return <div>
+            <h1>Innlegg</h1>
+                {innlegg.map(m =>
+                    <>
+                        <h2>{m.title} ({m.year})</h2>
+                        <div>{m.plot}</div>
+                    </>
+                    )}
+        </div>;
+}
+
 function Application() {
     return <BrowserRouter>
         <Routes>
             <Route path="/" element={<FrontPage/>}/>
             <Route path="/Innlegg/nytt" element={<h1>Nytt innlegg</h1>}/>
-            <Route path="/Innlegg/" element={<h1>Innlegg</h1>}/>
+            <Route path="/Innlegg/" element={<h1><ListInnlegg/></h1>}/>
         </Routes>
     </BrowserRouter>
 }
