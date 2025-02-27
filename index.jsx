c519159b7f83f2115334d9bdfbf476cce5288d5c
Henrikgg <Henrik_gg94@hotmail.com>
2025-02-27 23:47:27 +0100

    Added navigation with React Router

--- index.jsx
+++ index.jsx
@@ -1,8 +1,29 @@
 import * as React from 'react';
 import * as ReactDOM from 'react-dom';
+import { Route, Link, BrowserRouter, Routes} from "react-router-dom";
 
+
+function FrontPage() {
+        return <div>
+            <>Sosiale Medier Database</>
+            <ul>
+                <li><Link to="/Innlegg">Innlegg</Link></li>
+                <li><Link to="/Innlegg/nytt">Nye Innlegg</Link></li>
+            </ul>
+        </div>;
+}
+
+function Application() {
+    return <BrowserRouter>
+        <Routes>
+            <Route path="/" element={<FrontPage/>}/>
+            <Route path="/Innlegg/nytt" element={<h1>Nytt innlegg</h1>}/>
+            <Route path="/Innlegg/" element={<h1>Innlegg</h1>}/>
+        </Routes>
+    </BrowserRouter>
+}
 
 ReactDOM.render(
-    <h1>Sosiale Medier Database</h1>,
+    <h1><Application/></h1>,
     document.getElementById("app")
 );
