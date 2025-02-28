1cdceaa83e6ce654f43f8453c5a6be38689b0d12
Henrikgg <Henrik_gg94@hotmail.com>
2025-02-28 01:57:15 +0100

    liten express server

--- server/server.js
+++ server/server.js
@@ -2,6 +2,14 @@ import express from "express"
 
 const app = express();
 
+
+app.get("/api/innlegg", (req, res ) => {
+    res.json([
+        {title: "Hello :)"}
+    ])
+})
+
+
 const server = app.listen(3000,() => {
     console.log("Listening on http://localhost:" + server.address().port);
 });
